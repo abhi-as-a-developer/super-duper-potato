2df452ce7f5248ad51c8294c92f4643c4035a15e
Abhishek Banerjee <72243065+abhi-as-a-developer@users.noreply.github.com>
2021-02-12 20:43:10 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,44 @@
+<?php
+
+    ## sesssion start
+    session_start();
+
+    ## database connection
+    $hostdb = "localhost";
+    $userdb = "root";
+    $pwddb = "";
+    $namedb = "users";
+
+    $con = mysqli_connect($hostdb, $userdb, $pwddb, $namedb);
+
+    if ($con -> connect_error) {
+        die("Failed to connect to the databse " . $con -> connection_error);
+    }
+
+    ## getting info
+    $fullname = $_POST["full_name"];
+    $email = $_POST["email"];
+    $phone_number = $_POST["phone_number"];
+    $stream = $_POST["stream"];
+    $subject = $_POST["subject"];
+    $tution_mode = $_POST["tution_mode"];
+    $qualification = $_POST["qualification"];
+    $address = $_POST["address"];
+    $pwd = $_POST["password"];
+
+    ## checking if the email and phone number is already taken
+    $s = "SELECT * FROM users WHERE email = '$email' OR SELECT * FROM users WHERE phone_number = '$phone_number'";
+    // $p = "SELECT * FROM users WHERE phone_number = '$phone_number'";
+
+    $result = mysqli_query($con, $s);
+    $num = mysqli_num_rows($result);
+
+    if ($num == 1) {
+        echo"this email or phone number already registered, please try with a different one.";
+    }else{
+        $reg = "INSERT INTO users(fullname, email, phone_number, stream, subject, tution_mode, qualification, address, pwd) VALUES('$fullname', '$email', '$phone_number', '$stream', '$subject', '$tution_mode', '$qualification', '$address', '$pwd')";
+        mysqli_query($con, $reg);
+        echo"Registration done!";
+    }
+
+?>
